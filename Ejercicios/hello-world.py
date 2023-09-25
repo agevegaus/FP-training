693e1b3295fe93a0722cc8ec4caada3a9823d711
Agevega <agevega@hotmail.com>
2023-09-25 18:29:52 +0200

    .

--- Ejercicios/hello-world.py
+++ Ejercicios/hello-world.py
@@ -13,9 +13,11 @@ print(7 / 3)     # Division
 print(7 + 3.4)   # Addition of int and float
 
 # Print some errors.
+'''
 print(7 + "3")   # Addition of int and str
 print(7 + True)  # Addition of int and bool
 print(7 + 3j)    # Addition of int and complex
+'''
 
 print("Acabamos!")
 print("--------")
--- trash.py
+++ trash.py
@@ -1,1 +1,6 @@
-print("Empezamos...")
+
+# Crea una tupla
+tupla = (1, 2, 3, 4, 5)
+
+# Accede al primer elemento de la tupla
+print(tupla[0])
