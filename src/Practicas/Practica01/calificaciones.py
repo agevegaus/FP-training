4d73044f30c6faefea05e3bd6a401928aa2fa8e5
Agevega <agevega@hotmail.com>
2023-09-26 17:20:57 +0200

    Notas cuatrimestre

--- src/Practicas/Practica01/calificaciones.py
+++ src/Practicas/Practica01/calificaciones.py
@@ -1,14 +1,57 @@
 
+# Si nota es None devuelve 0, si no devuelve nota
 def a_cero(nota):
+    
     res = nota
+
     if nota == None:
-        res = 0
+        res = 0.0
+
     return res
 
+
+# Hace la media de las notas de teoría y devuelve 0 si es menor que 4
 def nota_teoria(nota1, nota2):
+
     t1 = a_cero(nota1)
     t2 = a_cero(nota2)
     res = (t1 + t2) / 2
+
     if res < 4:
-        res = 0
+        res = 0.0
+        
+    return res
+
+
+def nota_cuatrimestre(teoria, practico):
+    
+    t1 = a_cero(teoria[0])
+    t2 = a_cero(teoria[1])
+    p1 = a_cero(practico)
+
+    if nota_teoria(t1, t2) >= 4:
+        res = (0.1 * t1) + (0.1 * t2) + (0.8 * p1)
+    else:
+        res = 0.0
+
+    return res
+
+
+def nota_continua(tupla_teoricos, tupla_practicos):
+
+    t1 = a_cero(tupla_teoricos[0])
+    t2 = a_cero(tupla_teoricos[1])
+    t3 = a_cero(tupla_teoricos[2])
+    t4 = a_cero(tupla_teoricos[3])
+    p1 = a_cero(tupla_practicos[0])
+    p2 = a_cero(tupla_practicos[1])
+
+    c1 = nota_cuatrimestre((t1, t2), p1)
+    c2 = nota_cuatrimestre((t3, t4), p2)
+
+    if c1 >= 4 and c2 >= 4:
+        res = (c1 + c2) / 2
+    else:
+        res = min(4, (c1 + c2) / 2)
+
     return res
--- src/Practicas/Practica01/calificaciones_test.py
+++ src/Practicas/Practica01/calificaciones_test.py
@@ -1,12 +1,26 @@
 from calificaciones import *
 
 def main():
-    print("Prueba nota teoría")
+    print("Pruebas nota teoría")
     print(nota_teoria(9.1, 7.2))
     print(nota_teoria(4.0, 6.0))
     print(nota_teoria(4.0, 3.0))
     print(nota_teoria(None, 3.0))
     print(nota_teoria(9.0, None))
+
+    print("Pruebas nota cuatrimestre")
+    print(nota_cuatrimestre((9.1, 7.2), 8.1))
+    print(nota_cuatrimestre((4.0, 6.0), 5.0))
+    print(nota_cuatrimestre((4.0, 3.0), None))
+    print(nota_cuatrimestre((None, 3.0), None))
+    print(nota_cuatrimestre((9.0, None), 4.5))
+
+    print("Pruebas nota continua")
+    print(nota_continua((9.1, 7.2, 9.1, 7.2), (8.1, 8.1)))
+    print(nota_continua((4.0, 6.0, 4.0, 6.0), (5.0, 5.0)))
+    print(nota_continua((4.0, 3.0, 4.0, 3.0), (None, None)))
+    print(nota_continua((None, 3.0, None, 3.0), (None, None)))
+    print(nota_continua((9.0, None, 9.0, None), (4.5, 4.5)))
 
 if __name__ == "__main__":
     main()
