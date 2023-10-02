05684257e7bd513696ca97e433e78aec1b3db262
Agevega <agevega@hotmail.com>
2023-10-02 18:20:37 +0200

    folder data created

--- src/Ejercicios/005_ficheros.py
+++ src/Ejercicios/005_ficheros.py
@@ -1,3 +1,9 @@
-with open("./prueba.txt") as fichero:
+# Basic
+with open("./data/prueba.txt") as fichero:
+    for linea in fichero:
+        print(linea)
+
+# Encoding
+with open("./data/prueba.txt", encoding="utf-8") as fichero:
     for linea in fichero:
         print(linea)
